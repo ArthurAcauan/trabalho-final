6469e11a39370244d936c33ee098005338013cb5
ArthurAcauan <142455959+ArthurAcauan@users.noreply.github.com>
2023-09-21 20:58:24 -0300

    Update projeto.js

--- projeto.js
+++ projeto.js
@@ -1,64 +1,54 @@
-// inicio
+/****************************
+*ESTRUTURA BASE PARA UM MENU*
+*****************************/
 const reservas = [];
 
-// Função para criar uma nova reserva
 function criarReserva() {
-  const solicitante = prompt("Nome do solicitante:");
-  const data = prompt("Data da reserva (no formato dia-mes-ano):");
-  const horario = prompt("Horário da reserva:");
-  const sala = prompt("Sala desejada:");
-  const finalidade = prompt("Finalidade da reunião:");
-
-  const reserva = {
-    solicitante: solicitante,
-    data: data,
-    horario: horario,
-    sala: sala,
-    finalidade: finalidade,
-  };
-
- //array para armazenar todas as reservas
- const reservas = [];
-
-  // Armazena o índice da reserva
-  const indice = reservas.length;
-
-  // ADICIONA RESERVA NO ARRAY
-  reservas.push(reserva);
-  console.log(`Reserva criada com sucesso! Índice da reserva: ${indice}`);
-  console.log("--------------------");
+  // Implementar criação de reserva
 }
 
-// ... (resto do código)
+function visualizarReservas() {
+  // Implementar visualização de reservas
+}
 
-// Função principal para executar o programa
-function main() {
-  while (true) {
-    const opcao = prompt(
-        "Escolha uma opção: DIGITE; (1) Para criar Reserva, (2) Visualizar Reservas, (3) Editar Reserva, (4) Cancelar Reserva, (5) Sair."
-      );
+function editarReserva() {
+  // Implementar edição de reserva
+}
 
-    switch (opcao) {
-      case "1":
-        criarReserva();
-        break;
-      case "2":
-        visualizarReservas();
-        break;
-      case "3":
-        editarReserva();
-        break;
-      case "4":
-        cancelarReserva();
-        break;
-      case "5":
-        console.log("Saindo do programa.");
-        return;
-      default:
-        console.log("Opção inválida. Tente novamente.");
-    }
+function deletarReserva() {
+  // Implementar exclusão de reserva
+}
+
+function exibirMenu() {
+  console.log("Menu:");
+  console.log("1. Criar Reserva");
+  console.log("2. Visualizar Reservas");
+  console.log("3. Editar Reserva");
+  console.log("4. Deletar Reserva");
+  console.log("5. Sair");
+  const opcao = prompt("Digite o número da opção desejada:");
+
+  switch (opcao) {
+    case "1":
+      // Chamar função de criar reserva
+      break;
+    case "2":
+      // Chamar função de visualizar reservas
+      break;
+    case "3":
+      // Chamar função de editar reserva
+      break;
+    case "4":
+      // Chamar função de deletar reserva
+      break;
+    case "5":
+      console.log("Saindo do programa. Até logo!");
+      return;
+    default:
+      console.log("Opção inválida. Tente novamente.");
   }
+
+  // Chamar função de exibir menu novamente
 }
 
-// Iniciar o programa
-main();
+exibirMenu();
